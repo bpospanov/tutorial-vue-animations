f9f6d327fa93fe05b8ab5277e3a4605229eb7555
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2023-02-02 09:18:42 +0600

    5 - adding CSS keyframes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    <Toast v-if="showToast" />
+    <transition name="toast">
+      <Toast v-if="showToast" />
+    </transition>
     <Todos @badValue="triggerToast" />
   </div>
 </template>
@@ -26,5 +28,25 @@ export default {
 </script>
 
 <style>
+.toast-enter-active {
+  animation: wobble 0.5s ease;
+}
 
+.toast-leave-to {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+.toast-leave-active {
+  transition: all 0.2s ease;
+}
+
+@keyframes wobble {
+  0% { transform: translateY(-60px); opacity: 0 }
+  50% { transform: translateY(0px); opacity: 1 }
+  60% { transform: translateX(8px); }
+  70% { transform: translateX(-8px); }
+  80% { transform: translateX(4px); }
+  90% { transform: translateX(-4px); }
+  100% { transform: translateX(0); }
+}
 </style>
