86e44df10c22ab269299ebe91918077adbe65d67
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2023-02-13 12:52:09 +0600

    8 - transitioning between elements

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -6,14 +6,17 @@
       @keypress.enter="addTodo"
       placeholder="Add a new todo..."
     />
-    <div v-if="todos.length">
-      <transition-group tag="ul" name="todos-list" appear>
-        <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
-          {{ todo.text }}
-        </li>
-      </transition-group>
-    </div>
-    <div v-else>Woohoo, nothing left todo!</div>
+
+    <transition name="switch" mode="out-in">
+      <div v-if="todos.length">
+        <transition-group tag="ul" name="todos-list" appear>
+          <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
+            {{ todo.text }}
+          </li>
+        </transition-group>
+      </div>
+      <div v-else>Woohoo, nothing left todo!</div>
+    </transition>
   </div>
 </template>
 
@@ -81,6 +84,22 @@ export default {
 
   .todos-list-move {
     transition: transform 0.3s ease;
+  }
+
+  /* switch transitions */
+  .switch-enter-from, .switch-leave-to {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+
+  .switch-enter-to, .switch-leave-from {
+    opacity: 1;
+    transform: translateY(0);
+  }
+
+  .switch-enter-active,
+  .switch-leave-active {
+    transition: transform 0.5s ease;
   }
 
   input {
